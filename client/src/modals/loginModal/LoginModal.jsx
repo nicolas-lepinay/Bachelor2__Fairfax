3b7df760197f571bc323cd9965dfc65b94b3682f
nicolas-lepinay <nicolas.lepinay@outlook.com>
2021-11-24 08:43:29 +0100

    Progressed Login Modal

--- client/src/modals/loginModal/LoginModal.jsx
+++ client/src/modals/loginModal/LoginModal.jsx
@@ -1,86 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faEnvelope, faLock, faKey, faCheck } from "@fortawesome/free-solid-svg-icons";
+import KeyIcon from "../../svg/KeyIcon";
 
-import { ModalWrapper, ModalContainer, FormWrapper, Form, Title, InputAndIcon, Input, Info, Requirements, Text, Button, OverlayContainer, Overlay, Panel } from "./LoginModal.styled"
+import { ModalWrapper, ModalContainer, FormWrapper, Form, Title, InputAndIcon, LinkAndIcon, Input, Info, Requirements, Text, Button, OverlayContainer, Overlay, Panel, Description, SVG } from "./LoginModal.styled"
 import { MATERIAL_STYLE } from "./LoginModal.styled"
 
 function LoginModal() {
+
+    const [ active, setActive] = useState("");
+
+    const slidePanel = () => {
+        active === "" ? setActive("right__panel__active") : setActive("");
+    }
+
     return (
         <ModalWrapper>
-            <ModalContainer>
+            <ModalContainer className={active}>
                 <FormWrapper className="signup">
                     {/* <!-- | FORM SIGN-UP | --> */}
                     <Form>
-                        <Title>Register</Title>
+                        <Title className="golden">Register</Title>
             
                         {/* <!-- USERNAME INPUT --> */}
                         <InputAndIcon>
                             <FontAwesomeIcon icon={faUser} style={MATERIAL_STYLE} />
-                            <Input id="signup-username" type="text" name="signup-username" placeholder="Choose a username" pattern="^[ a-zA-Z0-9._]{3,20}" title="Only letters, numbers, spaces, dots and underscores. Length required: 3 ~ 20" required />
+                            <Input type="text" name="signup-username" placeholder="Choose a username" pattern="^[ a-zA-Z0-9._]{3,20}" title="Only letters, numbers, spaces, dots and underscores. Length required: 3 ~ 20" required />
                         </InputAndIcon>
             
                         {/* <!-- EMAIL INPUT --> */}
                         <InputAndIcon>
                             <FontAwesomeIcon icon={faEnvelope} style={MATERIAL_STYLE} />
-                            <Input id="signup-email" type="email" name="signup-email" placeholder="Enter your email address" maxlength="40" required />
+                            <Input type="email" name="signup-email" placeholder="Enter your email address" maxlength="40" required />
                         </InputAndIcon>
             
                         {/* <!-- PASSWORD (1) INPUT --> */}
                         <InputAndIcon>
                             <FontAwesomeIcon icon={faLock} style={MATERIAL_STYLE} />
-                            <Input id="signup-password-1" name="signup-password" type="password" placeholder="Choose a password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,30}" title="At least 6 characters, including a number, an uppercase letter and a lowercase letter." />
+                            <Input name="signup-password" type="password" placeholder="Choose a password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,30}" title="At least 6 characters, including a number, an uppercase letter and a lowercase letter." />
                         </InputAndIcon>
             
                         {/* <!-- PASSWORD (2) INPUT --> */}
                         <InputAndIcon>
                             <FontAwesomeIcon icon={faLock} style={MATERIAL_STYLE} />
-                            <Input id="signup-password-2" name="signup-password" type="password" placeholder="Confirm your password" required />
+                            <Input name="signup-password" type="password" placeholder="Confirm your password" required />
                         </InputAndIcon>
             
                         {/* <!-- AVERTISSEMENT "YOUR PASSWORDS DO NOT MATCH" --> */}
             
                         {/* <!-- INFOS SUR LES CARACTERES OBLIGATOIRES POUR LE MOT DE PASSE --> */}
                         <Info>
-                            <div>Your password must contain the following :</div>
+                            <div>Your password must contain:</div>
                             <Requirements>
-                                <svg id="svg-1" height="13" viewBox="0 0 16 13" width="16">
-                                    <path fill="#b19f6f" d="m5.6 8-3.2-3.2-2.4 2.4 5.6 5.6 10.4-10.4-2.4-2.4z"></path>
-                                </svg>
                                 <Text>A minimum of 6 characters.</Text>
                             </Requirements>
             
                             <Requirements>
-                                <svg id="svg-2" height="13" viewBox="0 0 16 13" width="16">
-                                    <path fill="#b19f6f" d="m5.6 8-3.2-3.2-2.4 2.4 5.6 5.6 10.4-10.4-2.4-2.4z"></path>
-                                </svg>
                                 <Text>Uppercase, lowercase, and a number.</Text>
                             </Requirements>
                         </Info>
             
-                        <Button class="button flat" id="signup-button" type="submit">Continue</Button>
+                        <Button class="button" type="submit">Continue</Button>
                     </Form>
                 </FormWrapper>
 
-                    <FormWrapper className="signin">
+                <FormWrapper className="signin">
                     <Form>
-                        <Title>Sign in</Title>
+                        <Title className="golden">Sign in</Title>
             
                         <InputAndIcon>
                             <FontAwesomeIcon icon={faUser} style={MATERIAL_STYLE} />
-                            <Input id="login-identifier" name="login-identifier" type="text" placeholder="Username or email address" required />
+                            <Input name="login-identifier" type="text" placeholder="Username or email address" required />
                         </InputAndIcon>
             
                         <InputAndIcon>
                             <FontAwesomeIcon icon={faLock} style={MATERIAL_STYLE} />
-                            <Input id="login-password" name="login-password" type="password" placeholder="Enter your password" required />
+                            <Input name="login-password" type="password" placeholder="Enter your password" required />
                         </InputAndIcon>
             
-                        <InputAndIcon>
+                        <LinkAndIcon>
+                            <SVG><KeyIcon/></SVG>
                             <a href="/forgotten-password">Forgot your password?</a>
-                        </InputAndIcon>
+                        </LinkAndIcon>
             
-                        <Button class="button flat" id="login-button">Continue</Button>
+                        <Button class="button" type="submit">Continue</Button>
                     </Form>
                 </FormWrapper>
 
@@ -88,13 +91,13 @@ function LoginModal() {
                     <Overlay>
                         <Panel className="left">
                             <Title>Already a member?</Title>
-                            <p>If you already hold a Wizard Passport delivered by the European Covendom, please sign in with your personal information.</p>
-                            <Button className="ghost" id="signIn">Sign In</Button>
+                            <Description>If you already hold a Wizard Passport delivered by the European Covendom, please sign in with your personal information.</Description>
+                            <Button className="ghost" onClick={slidePanel}>Sign In</Button>
                         </Panel>
                         <Panel className="right">
                             <Title>Not a member yet?</Title>
-                            <p>Apply for a Wizard Passport and begin your journey in the city of Fairfax.</p>
-                            <Button className="ghost" id="signUp">Sign Up</Button>
+                            <Description>Apply for a Wizard Passport and begin your journey in the city of Fairfax.</Description>
+                            <Button className="ghost" onClick={slidePanel}>Sign Up</Button>
                         </Panel>
                     </Overlay>
                 </OverlayContainer>
--- client/src/modals/loginModal/LoginModal.styled.jsx
+++ client/src/modals/loginModal/LoginModal.styled.jsx
@@ -1,24 +1,148 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+const ASSETS = process.env.REACT_APP_PUBLIC_ASSETS_FOLDER;
 
 const theme = {
     buttonColor: 'rgb(114,105,192)',
     buttonColorDarker: '#5349AD',
-    inputColor: 'rgb(248,247,255)',
+    inputColor: 'rgba(255, 253, 248, 0.3)',
     mainFontURL: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
     mainFont: '"Poppins", sans-serif',
     titleFontURL: 'https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet',
     titleFont: '"Crimson Text", serif',
-    goldColor: 'rgb(172, 155, 108)'
+    gold : {
+        color: 'rgb(172, 155, 108)',
+        gradient: `radial-gradient(circle,rgba(222, 208,160,1) 0%,rgba(167,150,102,1) 100%)`
+    }
 }
 
+const slide = keyframes`
+    0%,
+    49.99% {
+        opacity: 0;
+        z-index: 1;
+    }
+    50%,
+    100% {
+        opacity: 1;
+        z-index: 5;
+    }
+`
+
+const size = {
+    tablet_XL: '960px',
+    tablet_L: '830px',
+    tablet_M: '730px',
+    tablet_S: '630px',
+    mobile_XL: '500px',
+    mobile_L: '460px',
+    mobile_M: '400px'
+}
+
+const device = {
+    tablet_XL: `(max-width: ${size.tablet_XL})`,
+    tablet_L: `(max-width: ${size.tablet_L})`,
+    tablet_M: `(max-width: ${size.tablet_M})`,
+    tablet_S: `(max-width: ${size.tablet_S})`,
+    mobile_XL: `(max-width: ${size.mobile_XL})`,
+    mobile_L: `(max-width: ${size.mobile_L})`,
+    mobile_M: `(max-width: ${size.mobile_M})`
+};
+  
 export const MATERIAL_STYLE = {
-    color: theme.buttonColor,
+    color: "black",
     left: "20px",
     top: "49%",
-    opacity: 0.4,
+    opacity: "0.15",
     position: "absolute",
     transform: "translateY(-50%)"
 }
+
+export const SVG = styled.div`
+    color: rgba(0, 0, 0, 0.8s);
+    width: 0.9rem;
+    height: 0.9rem;
+    margin-right: 15px;
+    @media ${device.tablet_S} { 
+        display: none;
+    }
+`
+
+export const OverlayContainer = styled.div`
+    height: 100%;
+    left: 50%;
+    overflow: hidden;
+    position: absolute;
+    top: 0;
+    width: 50%;
+    z-index: 100;
+    transition: transform 0.6s ease-in-out;
+`
+
+export const Overlay = styled.div`
+    /* background: ${theme.gold.gradient}; */
+    background: ${theme.buttonColor};
+    color: white;
+    height: 100%;
+    left: -100%;
+    position: relative;
+    width: 200%;
+    transform: translateX(0);
+    transition: all 0.6s ease-in-out;
+
+    &:before {
+    content: ' ';
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.2;
+    background-image: url('${ASSETS}/landing_page/furniture2.svg');
+    background-repeat: no-repeat;
+    background-position: 0 0;
+    background-size: cover;
+    }
+`
+
+export const Panel = styled.div`
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0 40px;
+    text-align: center;
+    top: 0;
+    height: 100%;
+    width: 50%;
+    transform: translateX(0);
+    transition: transform 0.6s ease-in-out;
+    &.left {
+        transform: translateX(-20%);
+    }
+    &.right {
+        right: 0;
+        transform: translateX(0);
+    }
+    @media ${device.mobile_M} { 
+        padding: 0 20px;
+    }
+`
+
+export const Description = styled.p`
+    font-size: 1rem;
+    line-height: 20px;
+    letter-spacing: 0.5px;
+    margin: 20px 0 30px;
+
+    @media ${device.mobile_XL} { 
+        font-size: 0.8rem;
+    }
+    @media ${device.mobile_L} { 
+        display: none;
+    }
+`
 
 export const ModalWrapper = styled.div`
     @import url(${theme.mainFontURL});
@@ -34,7 +158,7 @@ export const ModalWrapper = styled.div`
 `
 
 export const ModalContainer = styled.div`
-    background-color: white;
+    background-color: rgb(251, 251, 251);
     border-radius: 15px;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     max-width: 90vw;
@@ -43,6 +167,31 @@ export const ModalContainer = styled.div`
     position: relative;
     overflow: hidden;
     width: 960px;
+    &.right__panel__active .signin {
+        transform: translateX(100%);
+    }
+    &.right__panel__active .signup {
+        transform: translateX(100%);
+        opacity: 1;
+        z-index: 5;
+        animation: ${slide} 0.6s;
+    }
+    &.right__panel__active ${OverlayContainer} {
+        transform: translateX(-100%);
+    }
+    &.right__panel__active ${Overlay} {
+        transform: translateX(50%);
+    }
+    &.right__panel__active .left {
+        transform: translateX(0);
+    }
+    &.right__panel__active .right {
+        transform: translateX(20%);
+    }
+
+    @media ${device.mobile_XL} { 
+        max-height: 500px;
+    }
 `
 
 export const FormWrapper = styled.div`
@@ -56,13 +205,13 @@ export const FormWrapper = styled.div`
         z-index: 2;
     }
     &.signup {
-        opacity: 0;
+        /* opacity: 0; */
         z-index: 1;
     }
 `
 
 export const Form = styled.form`
-    background-color: white;
+    background-color: rgb(251, 251, 251);
     display: flex;
     align-items: center;
     justify-content: center;
@@ -76,12 +225,54 @@ export const Title = styled.h1`
     @import url(${theme.titleFontURL});
     font-family: ${theme.titleFont};
     font-size: 2.3rem;
-    font-weight: bold;
+    font-weight: 600;
     margin-bottom: 20px;
+    &.golden {
+        font-size: 2.4rem;
+        background: #ac9b6c;
+        background-image: -webkit-radial-gradient(circle, #ded0a0 0%, #a79666 100%);
+        background-image: -moz-radial-gradient(circle, #ded0a0 0%, #a79666 100%);
+        background-image: -ms-radial-gradient(circle, #ded0a0 0%, #a79666 100%);
+        background-image: -o-radial-gradient(circle, #ded0a0 0%, #a79666 100%);
+        background-image: radial-gradient(circle, #ded0a0 0%, #a79666 100%);
+        color: transparent;
+        background-clip: text;
+        -webkit-background-clip: text;
+    }
+    &:not(.golden) {
+        text-shadow: 0 0 3px rgba(255, 255, 255, 0.6);
+    }
+
+    @media ${device.mobile_XL} { 
+        font-size: 1.8rem;
+    }
+    @media ${device.mobile_M} { 
+        font-size: 1.5rem;
+    }
 `
 
 export const InputAndIcon = styled.div`
     position: relative;
+`
+
+export const LinkAndIcon = styled.div`
+    align-items: baseline;
+    display: flex;
+    justify-content: center;
+    opacity: 0.5;
+    margin: 10px 0 80px 0;
+    & a {
+        color: black;
+        font-size: 0.8rem;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    @media ${device.tablet_S} { 
+        margin: 10px 0 40px 0;
+
+    }
 `
 
 export const Input = styled.input`
@@ -103,6 +294,18 @@ export const Input = styled.input`
         opacity: 0.5;
         text-transform: uppercase;
     }
+
+    @media ${device.tablet_S} { 
+        padding: 12px 12px 12px 40px;
+        max-width: 180px;
+        min-width: 50px;
+        &::placeholder {
+            font-size:70%;
+        }    
+    }
+    @media ${device.mobile_XL} { 
+        min-width: 50px;
+    }
 `
 
 export const Info = styled.div`
@@ -110,16 +313,36 @@ export const Info = styled.div`
     text-shadow: 0 0 2px rgba(255, 255, 255, 0.5);
     margin-top: 20px;
     margin-bottom: 40px;
+
+    @media ${device.tablet_S} { 
+        display: none;  
+    }
 `
 
 export const Requirements = styled.div`
     position: relative;
     margin-left: 0px;
+
+    @media ${device.tablet_XL} { 
+        margin-left: 0px;
+    }
+
+    @media ${device.tablet_L} { 
+        margin-left: -10px;
+    }
+
+    @media ${device.tablet_M} { 
+        margin-left: -20px;
+    }
 `
 
 export const Text = styled.span`
     float: left;
     padding-left: 30px;
+
+    @media ${device.tablet_XL} { 
+        padding-left: 20px;
+    }
 `
 
 export const Button = styled.button`
@@ -145,54 +368,32 @@ export const Button = styled.button`
         &:hover {
             background-color: rgba(255, 255, 255, 0.1);
         }
+        @media ${device.tablet_S} { 
+            margin-top: 10px; 
+        }
+        @media ${device.mobile_XL} { 
+            padding: 12px 30px;
+        }
+        @media ${device.mobile_L} { 
+            margin-top: 50px;
+        }
+    }
+
+    @media ${device.tablet_S} { 
+        border-radius: 50px;
+        color: #ffffff;
+        cursor: pointer;
+        font-size: 9px;
+        font-weight: bold;
+        padding: 12px 25px;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+    }
+
+    &:not(.ghost) {
+        @media ${device.tablet_S} { 
+            margin-top: 50px;
+        }
     }
 `
 
-export const OverlayContainer = styled.div`
-    height: 100%;
-    left: 50%;
-    overflow: hidden;
-    position: absolute;
-    top: 0;
-    width: 50%;
-    z-index: 100;
-    transition: transform 0.6s ease-in-out;
-`
-
-export const Overlay = styled.div`
-    color: white;
-    height: 100%;
-    left: -100%;
-    position: relative;
-    width: 200%;
-    transform: translateX(0);
-    transition: transform 0.6s ease-in-out;
-
-    background: ${theme.goldColor};
-    background: radial-gradient(circle,rgba(222, 208,160,1) 0%,rgba(167,150,102,1) 100%);
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: 0 0;
-`
-
-export const Panel = styled.div`
-    position: absolute;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    padding: 0 40px;
-    text-align: center;
-    top: 0;
-    height: 100%;
-    width: 50%;
-    transform: translateX(0);
-    transition: transform 0.6s ease-in-out;
-    &.left {
-        transform: translateX(-20%);
-    }
-    &.right {
-        right: 0;
-        transform: translateX(0);
-    }
-`
