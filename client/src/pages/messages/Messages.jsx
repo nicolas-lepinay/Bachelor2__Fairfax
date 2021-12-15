4355a089e75782aaa0c0ae211b2eca3e69896cf9
quentin <q.mayer@hexa.solutions>
2021-12-15 10:48:48 +0100

    messages

---
+++ client/src/pages/messages/Messages.jsx
@@ -0,0 +1,128 @@
+import "./messages.css";
+
+export default function Messages() {
+  return (
+    <div className="page--message">
+      <div className="mainContainer">
+        <div className="leftContainer">
+          <div className="messageContainer">
+            <div className="messageContent">
+              <img
+                className="profilImage"
+                height="80px"
+                width="80px"
+                src="../static/images/index/sky1_purple.webp"
+              />
+              <div className="textContainer">
+                <span className="nameText">John Doe</span>
+                <span className="messageText">
+                  message dokzodzjod ofejfepooj feofj eofj fjeof jeof ejof
+                </span>
+              </div>
+            </div>
+            <span className="timeText">12:45</span>
+          </div>
+        </div>
+        <div className="rightContainer">
+          <div className="chatContainer">
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+            <p className="from-them">
+              It was loud. We just laid there and said &ldquo;is this an
+              earthquake? I think this is an earthquake.&rdquo;
+            </p>
+            <p className="from-me">
+              Like is this an earthquake just go back to sleep
+            </p>
+          </div>
+          <div className="inputContainer">
+            <input
+              type="text"
+              id="fname"
+              name="firstname"
+              placeholder="Message..."
+            />
+            <button className="buttonStyle">
+              <span>Envoyer</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
