192c6d15116648e1b277406c1198df3965223e42
nicolas-lepinay <nicolas.lepinay@outlook.com>
2022-01-12 09:35:25 +0100

    (Test)

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -50,7 +50,6 @@ export default function Profile() {
                         <div className="profileInfo">
                             <h4 className="profileInfoName">{user.username}</h4>
                             <span className="profileInfoDesc">Description</span>
-
                         </div>
                     </div>
                     <div className="profileRightBottom">
