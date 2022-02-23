ded0ef57c6563e52489eb5c474edf9bc7894d17c
Ianis PARFAIT <ianis.google@formationfim.onmicrosoft.com>
2022-02-23 14:41:52 +0100

    profile

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -10,7 +10,6 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import "./profile.css"
 
 export default function Profile() {
-
     const ASSETS = process.env.REACT_APP_PUBLIC_ASSETS_FOLDER;
     const MEDIA = process.env.REACT_APP_PUBLIC_MEDIA_FOLDER;
 
@@ -23,36 +22,96 @@ export default function Profile() {
             setUser(res.data);
         }
         fetchUser();
-    }, [username])
+    }, [username]);
 
 
     return (
         <>
-            <Topbar/>
-            <div className="profile">
-                <Sidebar/>
-                <div className="profileRight">
-                    <div className="profileRightTop">
-                        <div className="profileCover">
-                            <img className="profileCoverImg" src={`${MEDIA}/post/0.jpg`} alt="" />
-                            <div className="avatar">
-                                <img className="frame" src={`${ASSETS}/golden_frame.webp`} alt="" />
-                                <img className="profileUserImg"src={user.avatar ? `${MEDIA}/${user.avatar}` : `${MEDIA}/profile/defaultAvatar.jpg`} alt="" />
-                            </div>
-                        </div>
-                        <div className="profileInfo">
-                            <h4 className="profileInfoName">{user.username}</h4>
-                            <span className="profileInfoDesc">Description</span>
-
-                        </div>
-                       
-                    </div>
-                    <div className="profileRightBottom">
-                        <Feed username={username} />
-                        <Rightbar user={user}/>
-                    </div>
-                </div>
-            </div>
+				<Topbar/>
+					<div className="profileRight container_profile">
+						<img className="profileCoverImg cover_bg" src={`${MEDIA}/post/0.jpg`} alt="" />
+						<div className="profileRightTop" id="fullpage">
+							<div className="profileCover section">
+									<img className="avatar_house" src={user.avatar ? `${MEDIA}/${user.avatar}` : `${MEDIA}/profile/defaultAvatar.jpg`} />
+									<div className="avatar">
+											{/* <img className="frame" src={`${ASSETS}/golden_frame.webp`} alt="" /> */}
+											<img className="profileUserImg"src={user.avatar ? `${MEDIA}/${user.avatar}` : `${MEDIA}/profile/defaultAvatar.jpg`} alt="" />
+											<h1 className="username">{user.username}</h1>
+										</div>
+									<span className="name_house">Nom de la maison</span>
+							</div>
+							<div className="profileInfo section">
+								<h2 className="activity_title">Activity</h2>
+								<div className="container_activs">
+									<div className="activity_faxes">
+										<div className="header"><span className="title">Your Faxes</span></div>
+										<div className="content">
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+											<div className="item_content">
+												<span className="title_item">Test titre item</span>
+												<span className="date_item">October 2021, 12th</span>
+											</div>
+										</div>
+									</div>
+									<div className="activity_replies">
+										<div className="header"><span className="title">Your Replies</span></div>
+										<div className="content"></div>
+									</div>
+									<div className="activity_faxes_likes">
+										<div className="header"><span className="title">Faxes you liked</span></div>
+										<div className="content"></div>
+									</div>
+									<div className="activity_replies_liked">
+										<div className="header"><span className="title">Replies you liked</span></div>
+										<div className="content"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
         </>
     )
 }
